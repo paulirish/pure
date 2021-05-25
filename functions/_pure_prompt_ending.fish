36f7f95fb5a8d96823736fdf46aa7ec933851e7a
Paul Irish <commits@paul.irish>
2021-05-25 11:52:19 -0700

    use non-space after prompt symbol so i dont exclude anything from my history.

--- functions/_pure_prompt_ending.fish
+++ functions/_pure_prompt_ending.fish
@@ -1,3 +1,4 @@
 function _pure_prompt_ending
-    echo "$pure_color_normal "
+    #echo "$pure_color_normal "
+    echo "$pure_color_normal "  # this whitespace character is an EM QUAD. this way i dont lose space-prefixed commands to history/histcontrol
 end
